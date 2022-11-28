432a21c5ae293e8e4942791d55701eaf79a6691b
Mcorduk <corduk.5tp@gmail.com>
2022-11-28 12:18:33 +0300

    Complete and pass all test in 01 and 02 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
-
-// Do not edit below this line
-module.exports = repeatString;
+const repeatString = function(string,num) {
+    let result = "";
+   
+    if(num < 0){
+       result = "ERROR"
+       return result;
+    } else{
+       for(i = 0; i < num; i++){
+           result = result.concat(string);
+       }
+       return result;
+   }};
+   
+   // Do not edit below this line
+   module.exports = repeatString;
+   
