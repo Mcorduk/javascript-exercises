79882a65833cf2a406ac6b9c75e17181d1b32ae7
Mcorduk <corduk.5tp@gmail.com>
2023-01-14 19:03:30 +0300

    First draft of the solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,11 @@
 const findTheOldest = function() {
+  let newArray = people.map(function(person) {
+    person.age = person.yearOfDeath - person.yearOfBirth;
+    return person;
+});
 
-    
-
+    let oldest = newArray.reduce((accumulator, currentValue) => accumulator.age > currentValue.age ? accumulator : currentValue);
+    return oldest.name;
 };
 
 // Do not edit below this line
